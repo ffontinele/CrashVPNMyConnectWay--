c6d8eda4b6bc6fa9fc060ca5b5761bca93438348
ffontinele <42858360+ffontinele@users.noreply.github.com>
2022-04-07 20:11:20 -0300

    Update delhost.sh

--- Modulos/delhost.sh
+++ Modulos/delhost.sh
@@ -7,24 +7,24 @@ fi
 tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-10s\n' "Remover Host do Squid Proxy" ; tput sgr0
 if [ ! -f "$payload" ]
 then
-	tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Arquivo $payload nÃ£o encontrado" ; tput sgr0
+	tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Arquivo $payload nao encontrado" ; tput sgr0
 	exit 1
 else
-	tput setaf 2 ; tput bold ; echo ""; echo "DomÃ­nios atuais no arquivo $payload:" ; tput sgr0
+	tput setaf 2 ; tput bold ; echo ""; echo "Dominios atuais no arquivo $payload:" ; tput sgr0
 	tput setaf 3 ; tput bold ; echo "" ; cat $payload ; echo "" ; tput sgr0
-	read -p "Digite o domÃ­nio que deseja remover da lista: " host
+	read -p "Digite o dominio que deseja remover da lista: " host
 	if [[ -z $host ]]
 		then
-			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "VocÃª digitou um domÃ­nio vazio ou nÃ£o existente!" ; echo "" ; tput sgr0
+			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "Voce digitou um dominio vazio ou nao existente!" ; echo "" ; tput sgr0
 			exit 1
 		else
 		if [[ `grep -c "^$host" $payload` -ne 1 ]]
 		then
-			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "O domÃ­nio $host nÃ£o foi encontrado no arquivo $payload" ; echo "" ; tput sgr0
+			tput setaf 7 ; tput setab 4 ; tput bold ; echo "" ; echo "O dominio $host nao foi encontrado no arquivo $payload" ; echo "" ; tput sgr0
 			exit 1
 		else
 			grep -v "^$host" $payload > /tmp/a && mv /tmp/a $payload
-			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "Arquivo $payload atualizado, o domÃ­nio foi removido com sucesso:" ; tput sgr0
+			tput setaf 7 ; tput setab 1 ; tput bold ; echo "" ; echo "Arquivo $payload atualizado, o dominio foi removido com sucesso:" ; tput sgr0
 			tput setaf 3 ; tput bold ; echo "" ; cat $payload ; echo "" ; tput sgr0
 			if [ ! -f "/etc/init.d/squid3" ]
 			then
