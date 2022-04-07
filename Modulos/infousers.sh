170a4a9aedff2974e2cfae4576335a42f1fdb7d4
ffontinele <42858360+ffontinele@users.noreply.github.com>
2022-04-07 20:22:48 -0300

    Update infousers.sh

--- Modulos/infousers.sh
+++ Modulos/infousers.sh
@@ -35,7 +35,7 @@ Senha=$(printf '%-13s' "$senha")
 Limite=$(printf '%-10s' "$lim")
 Data=$(printf '%-1s' "$data")
 echo -e "\033[1;33m$Usuario \033[1;37m$Senha \033[1;37m$Limite \033[1;32m$Data\033[0m"
-echo -e "\033[0;34mâ”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”â”\033[0m"
+echo -e "\033[0;34m===========================================================\033[0m"
 done
 echo ""
 _tuser=$(awk -F: '$3>=1000 {print $1}' /etc/passwd | grep -v nobody | wc -l)
@@ -44,4 +44,4 @@ _ons=$(ps -x | grep sshd | grep -v root | grep priv | wc -l)
 [[ -e /etc/openvpn/openvpn-status.log ]] && _onop=$(grep -c "10.8.0" /etc/openvpn/openvpn-status.log) || _onop="0"
 [[ -e /etc/default/dropbear ]] && _drp=$(ps aux | grep dropbear | grep -v grep | wc -l) _ondrp=$(($_drp - 1)) || _ondrp="0"
 _onli=$(($_ons + $_onop + $_ondrp))
-echo -e "\033[1;33mâ€¢ \033[1;36mTOTAL USUARIOS\033[1;37m $_tuser \033[1;33mâ€¢ \033[1;32mONLINES\033[1;37m: $_onli \033[1;33mâ€¢ \033[1;31mVENCIDOS\033[1;37m: $_expuser \033[1;33mâ€¢\033[0m"
+echo -e "\033[1;33m**** \033[1;36mTOTAL USUARIOS\033[1;37m $_tuser \033[1;33m**** \033[1;32mONLINES\033[1;37m: $_onli \033[1;33m**** \033[1;31mVENCIDOS\033[1;37m: $_expuser \033[1;33m****\033[0m"
